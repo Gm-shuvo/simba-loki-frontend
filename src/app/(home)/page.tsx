07b7d6064e6a709f25f5d1909ee308488ccff1ae
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2026-05-13 00:10:01 +0600

    feat: refactor hero section with responsive design and add feature highlights component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { HeroSection } from "@/components/home/hero/hero-section";
+
+export default function HomePage() {
+  return (
+    <div>
+      <HeroSection />
+      <div className="px-6 md:px-10 my-20">
+        Section
+      </div>
+    </div>
+  );
+}
